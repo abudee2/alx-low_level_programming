352552817c5e3bd554940ac9d82d3807b35ca897
abudee2 <81663746+abudee2@users.noreply.github.com>
2022-10-24 17:01:32 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
+
 /**
  * main - Entry point
- * Desc: print "_putchar" followed by a newline
- * Return: 0 on success
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-	int n = 0;
-	char alx[10] = "_putchar\n";
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	while (n < 9)
-	{
-		_putchar(alx[n]);
-		n++;
-	}
 	return (0);
 }
