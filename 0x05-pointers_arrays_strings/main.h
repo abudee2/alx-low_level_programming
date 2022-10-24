53dce7bdfb2d9d0eb446d04f7a991244d00c6620
abudee2 <abdullahi4yakubu@gmail.com>
2022-10-24 05:32:36 -0700

    task o

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef main_H
 #define main_H
+
 void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
@@ -12,4 +13,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-#endif /*main.h*/
+
+#endif
