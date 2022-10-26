9d1ec29ad16b39167824ef79ad7b0134827b0141
abudee2 <81663746+abudee2@users.noreply.github.com>
2022-10-26 13:12:26 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,40 +1,33 @@
-#include "main.h"
 #include <stdio.h>
-
 /**
-  * main - Prints a Fizz Buzz program
-  *
-  * Return: Always 0 (Success)
-  */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int x;
+	int m;
 
-	for (x = 1; x <= 100; x++)
+	for (m = 1 ; m < 100 ; m++)
 	{
-		if ((x % 3 == 0) && (x % 5 == 0))
+		if ((m % 3) == 0 && (m % 5) == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
-		else if (x % 3 == 0)
+		else if (m % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
-		else if (x % 5 == 0)
+		else if (m % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d", x);
-		}
-
-		if (x != 100)
-		{
-			printf(" ");
+			printf("%d ", m);
 		}
 	}
-
+	printf("Buzz");
 	printf("\n");
-
 	return (0);
+}
